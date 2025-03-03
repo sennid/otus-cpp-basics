56a933704d42042b1abaa16a94943818d0ee4a58
Test User <sennid@test.com>
2025-03-03 23:45:42 +0300

    add parameters

--- src/guess_the_number.cpp
+++ src/guess_the_number.cpp
@@ -2,17 +2,26 @@
 
 #include "check_value.h"
 #include "random_value.h"
+#include "argument.h"
 
 
-int guessNumber() {
+int guessNumber(const int max)
+{
 
-    int guess_value = random_value(100);
+    int guess_value = random_value(max);
     return checkValue(guess_value);
     
 }
 
-int main ()
+int main (int argc, char** argv)
 {
     std::cout << "-=Guess The Number Game=-" << std::endl;
-    guessNumber();
+
+    int arg = argument(argc, argv);
+    if (arg > 0)
+    {
+        guessNumber(arg);
+    }
+
+    return 0;
 }
